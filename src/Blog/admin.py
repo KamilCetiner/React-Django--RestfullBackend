3b75ebaac7eca4a8719dab7d9e2499790b9d0730
KamilCetiner <cetiner.kamil84@gmail.com>
2021-01-20 12:41:04 +0100

    models created

--- src/Blog/admin.py
+++ src/Blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Like , Comment , View
+
+
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(View)
 
 # Register your models here.
